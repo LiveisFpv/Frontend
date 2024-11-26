ef018f93c2a0c80ab2ae2c667a1e9e6bb25ac570
LiveisFpv <nasonovnicolai@gmail.com>
2024-11-26 21:09:53 +0300

    Пупу

--- ИДЗ/script.js
+++ ИДЗ/script.js
@@ -4,15 +4,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
     slider.addEventListener("input", () => {
         const value = slider.value;
+        var lastValue = 0
         console.log(value);
         if (value == 100){
             slider_cont=document.getElementById("slider-container");
             slider_cont.style.display="none"
         }
-
+        if (value < lastValue) {
+            slider.value = lastValue;
+            return;
+        }
+        lastValue = value;
         const offset = -100 + (value * 100)/100;
 
         content.style.transform=`translateX(${offset}%)`;
+        const percentage = (value * 100) / slider.max;
+        slider.style.background = `linear-gradient(to right, transparent ${percentage}%, #4caf50 ${percentage}%)`;
         console.log(content.style.transform);
     });
 });
